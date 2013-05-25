cd9e1762ea29c0bcfa0eabeb4ff657df8220c66a
Thorsten Lorenz <thlorenz@gmx.de>
2013-05-25 13:31:23 -0400

    create read delete tests

---
+++ test/crd.js
@@ -0,0 +1,45 @@
+'use strict';
+/*jshint asi: true */
+
+// Tests only work in author's environment since password is required to execute them
+
+var test = require('tap').test
+var getHook = require('../lib/get-testling-hook')
+  , createHook = require('../lib/create-testling-hook')
+  , deleteHook = require('../lib/delete-testling-hook')
+  , testHook = require('../lib/test-testling-hook')
+  , pwd = process.env.githubpwd
+  ;
+
+test('given a no testling hook exists for a repo', function (t) {
+  t.plan(7)
+
+  t.once('end', function () {
+    // cleanup
+    deleteHook('thlorenz', pwd, 'test-api', function (err, res) { t.end() })
+  })
+
+  deleteHook('thlorenz', pwd, 'test-api', function (err, res) {
+    // may have err if no hook existed, but we don't care
+
+    testHook('thlorenz', pwd, 'test-api', function (err, res) {
+      t.ok(err, 'testing hook has error')
+    })
+
+    createHook('thlorenz', pwd, 'test-api', function (err, res) {
+
+      t.notOk(err, 'no error on first creation')
+      t.ok(res.created, 'creates hook')
+
+      testHook('thlorenz', pwd, 'test-api', function (err, res) {
+        t.notOk(err, 'testing hook has no error')
+        t.ok(res.sent, 'post sent')
+      })
+
+      createHook('thlorenz', pwd, 'test-api', function (err, res) {
+        t.notOk(err, 'no error on second creation')
+        t.notOk(res.created, 'not created again')
+      })
+    })
+  })
+})
