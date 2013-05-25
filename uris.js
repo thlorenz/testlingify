d123c8d125509315bb7e3300ba6fdab0f53684f6
Thorsten Lorenz <thlorenz@gmx.de>
2013-05-24 22:46:39 -0400

    improving credentials in uri

--- uris.js
+++ uris.js
@@ -2,6 +2,11 @@
 
 var protocol = exports.protocol = 'https://';
 var api = exports.api = 'api.github.com';
+
+var credentials = exports.credentials = function (uname, pwd) {
+  return [uname, pwd].map(encodeURIComponent).join(':');
+};
+
 exports.hooks = function (uname, pwd, repo) {
-  return protocol + uname + ':' + pwd + '@' + api + '/repos/' + uname + '/' + repo + '/hooks';
+  return protocol + credentials(uname, pwd) + '@' + api + '/repos/' + uname + '/' + repo + '/hooks';
 };
