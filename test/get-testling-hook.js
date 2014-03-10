8741cb49b5e30f130c88042229e6c94753495cc5
Thorsten Lorenz <thlorenz@gmx.de>
2014-03-10 16:28:47 -0400

    fixing hook tests and adding more information on how to run them

--- test/get-testling-hook.js
+++ test/get-testling-hook.js
@@ -2,14 +2,23 @@
 /*jshint asi: true */
 
 // Tests only work in author's environment since password is required to execute them
+// NOTE: change/remove this if you want to run these tests
 if (process.env.USER !== 'thlorenz') return;
 
 var test = require('tap').test
 var getHook = require('../lib/get-testling-hook')
 
+function inspect(obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true));
+}
+
+// NOTE: in order to make these tests pass, please change thlorenz/brace and thlorenz/proxyquire to
+// repos use testling and/or at least that you have access to
+// Additionally set $GITHUB_PWD in your environment to your github password
+
 test('get testling hook existing repo with testling hook', function (t) {
   getHook('thlorenz', process.env.GITHUB_PWD, 'thlorenz', 'brace', function (err, hook) {
-    t.notOk(err, 'no error')
+    if (err) { inspect(err); t.fail(err); return t.end(); }
     t.equals(hook.config.url, 'http://git.testling.com')
     t.end()
   });
@@ -17,7 +26,7 @@ test('get testling hook existing repo with testling hook', function (t) {
 
 test('get testling hook existing repo without testling hook', function (t) {
   getHook('thlorenz', process.env.GITHUB_PWD, 'thlorenz', 'proxyquire', function (err, hook) {
-    t.notOk(err, 'no error')
+    if (err) { inspect(err); t.fail(err); return t.end(); }
     t.notOk(hook, 'no hook')
     t.end()
   });
